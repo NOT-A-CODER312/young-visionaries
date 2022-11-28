216242f17d3843e0de8bd1b2addfcd7eb1036e4f
Shanara Watts <watts_shan2637@live.com>
2022-11-27 22:19:03 -0500

    Suggestion Box drop down

--- components/EventsList/EventSuggest.js
+++ components/EventsList/EventSuggest.js
@@ -1,14 +1,23 @@
 import classes from "./EventSuggest.module.css";
 import React from "react";
-import Select2 from "react-select2";
+
 
 export default function EventSuggest() {
+
     return (
         <div className={classes.suggest}>
             <form action="/send-data-here" method="post">
                 <label className={classes.label} for="etype">Event Type:</label>
                 <br />
-                <input className={classes.input1} type="text" id="etype" name="etype" required maxLength="30"  />
+                <select>
+                    <option disabled>Select Type</option>
+                    <option>Fundraiser</option>
+                    <option>Party</option>                    
+                    <option>Festival</option>
+                    <option>Confrence</option>
+                    <option>Sport</option>
+                </select>
+                {/*<input className={classes.input1} type="text" id="etype" name="etype" required maxLength="30"  /> */}
                 <br /><br />
                 <label className={classes.label} for="purpose">Purpose of the Event:</label>
                 <br />
