14da164d422a167e6fede616698bfa836977b2de
Shanara Watts <watts_shan2637@live.com>
2022-11-27 20:00:15 -0500

    Suggestion Box 1

--- components/EventsList/EventSuggest.js
+++ components/EventsList/EventSuggest.js
@@ -6,13 +6,13 @@ export default function EventSuggest() {
     return (
         <div className={classes.suggest}>
             <form action="/send-data-here" method="post">
-                <label for="etype">Event Type:</label>
+                <label className={classes.label} for="etype">Event Type:</label>
                 <br />
-                <input type="text" id="etype" name="etype" required minLength="10" maxLength="30"  />
+                <input className={classes.input1} type="text" id="etype" name="etype" required maxLength="30"  />
                 <br /><br />
-                <label for="purpose">Purpose of the Event:</label>
+                <label className={classes.label} for="purpose">Purpose of the Event:</label>
                 <br />
-                <input type="text" id="purpose" name="purpose" required minLength="20" maxLength="100" />
+                <input className={classes.input2} type="text" id="purpose" name="purpose" required minLength="20" maxLength="100" />
                 <br /><br />
                 <button type="submit">Submit</button>
             </form>
